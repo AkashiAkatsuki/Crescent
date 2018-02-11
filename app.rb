15069d51ce6aa179a214cdf8e9e490b5ce5ee7db
AkashiAkatsuki <katsuki@sakplus.jp>
2018-02-12 00:31:48 +0900

    coreにリクエストを渡したらだいたい完成した

--- app.rb
+++ app.rb
@@ -3,9 +3,10 @@ require './core.rb'
 require 'json'
 
 before do
+  @core = Core.new
 end
 
 post '/api/talk' do
   content_type 'application/json'
-  return { text: params[:text] }.to_json
+  return { text: @core.response(params[:text]) }.to_json
 end
