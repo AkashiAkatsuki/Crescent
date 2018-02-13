ced20f38484130e06b5ed8609aafe67c176a5438
AkashiAkatsuki <katsuki@sakplus.jp>
2018-02-13 23:44:11 +0900

    名前とユーザー名必須にした

--- app.rb
+++ app.rb
@@ -1,6 +1,8 @@
+# coding: utf-8
 require 'sinatra'
 require './core.rb'
 require 'json'
+require 'pry'
 
 before do
   @core = Core.new
@@ -8,5 +10,7 @@ end
 
 post '/api/talk' do
   content_type 'application/json'
-  return { text: @core.response(params[:text]) }.to_json
+  return { text: '無理...' }.to_json unless params.key? :text
+  return { text: '誰？'}.to_json unless params.key?(:name) && params.key?(:screen_name) 
+  { text: @core.response(params[:text], member: params[:name], screen_name: params[:screen_name]) }.to_json
 end
